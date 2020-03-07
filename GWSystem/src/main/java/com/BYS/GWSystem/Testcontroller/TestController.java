e059e1ef820b9f169b6c12d67c0d6dc24ab6c4d4
Jerry-zjj <1393424504@qq.com>
2020-03-07 23:23:42 +0800

    test

--- GWSystem/src/main/java/com/BYS/GWSystem/Testcontroller/TestController.java
+++ GWSystem/src/main/java/com/BYS/GWSystem/Testcontroller/TestController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
 	@RequestMapping("caonima")
 		public String show() {
-			return "wo cao ni ma de bi";
+			return "wo cao ni ma de bi ";
 		}
 
 }
