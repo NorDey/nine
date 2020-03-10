888f2fdb632a3cde5c3736ccfc0750868d324f38
Jerry-zjj <1393424504@qq.com>
2020-03-11 00:15:49 +0800

    注释添加,无用数据去除,编写单表增删改查

--- GWSystem/src/main/java/com/BYS/GWSystem/mapper/PostMapper.java
+++ GWSystem/src/main/java/com/BYS/GWSystem/mapper/PostMapper.java
@@ -1,8 +1,24 @@
 package com.BYS.GWSystem.mapper;
 
+import java.util.List;
+
 import org.apache.ibatis.annotations.Mapper;
+
+import com.BYS.GWSystem.model.Post;
 
 @Mapper
 public interface PostMapper  {
+	
+			//查询岗位
+			public List<Post> selectPostList();
+			
+			//更改
+			public int updatePost(Post post);
+			
+			//添加
+			public int insertPost(Post post);
+			
+			//删除
+			public int deletePost(Post post);
 
 }
