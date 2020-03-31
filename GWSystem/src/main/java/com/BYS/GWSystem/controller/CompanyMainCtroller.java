a3c0a8b7796bc13bfda120f0ae9cec67993b2c67
NorDey <yaozhenbang@icloud.com>
2020-03-31 17:18:21 +0800

    Controller做成

--- GWSystem/src/main/java/com/BYS/GWSystem/controller/CompanyMainCtroller.java
+++ GWSystem/src/main/java/com/BYS/GWSystem/controller/CompanyMainCtroller.java
@@ -5,13 +5,57 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-@ResponseBody
 @Controller
 public class CompanyMainCtroller {
 	
-	@GetMapping("/CompanySwitchLog")
-	public String Log(Model model) {
-		return "admin/SwitchLogin";
+	@GetMapping("/CC")//公司收藏
+	public String CC(Model model) {
+		return "admin/CompanyCollection";
+	}
+	
+	@GetMapping("/CHIC")//公司修改招聘信息
+	public String CHIC(Model model) {
+		return "admin/CompanyHiredInfoChange";
+	}
+	
+	@GetMapping("/CHIQ")//公司招聘的详情（给未登录看）
+	public String CHIQ(Model model) {
+		return "admin/CompanyHiredInfoQuality";
+	}
+	
+	@GetMapping("/CHIS")//公司招聘信息简要列表（给未登录看）
+	public String CHIS(Model model) {
+		return "admin/CompanyHiredInfoToShow";
+	}
+	
+	@GetMapping("/CH")//公司浏览简历的历史记录
+	public String CH(Model model) {
+		return "admin/CompanyHistory";
+	}
+	
+	@GetMapping("/CI")//公司的主页信息（可以修改）
+	public String CI(Model model) {
+		return "admin/CompanyInfo";
+	}
+	
+	@GetMapping("/CIS")//公司的主页信息（给别人看，不可以跳转修改）
+	public String CIS(Model model) {
+		return "admin/CompanyInfoToShow";
+	}
+	
+	@GetMapping("/CM")//公司招聘信息简要列表（可以修改）
+	public String CM(Model model) {
+		return "admin/CompanyManger";
+	}
+	
+	@GetMapping("/CNH")//公司新建招聘信息
+	public String CNH(Model model) {
+		return "admin/CompanyNewHired";
+	}
+	
+	@GetMapping("/CRCI")//公司主页信息修改页面
+	public String CRCI(Model model) {
+		return "admin/CompanyRebuildCompanyInfo";
 	}
 	
 }
--- GWSystem/src/main/java/com/BYS/GWSystem/controller/PublicController.java
+++ GWSystem/src/main/java/com/BYS/GWSystem/controller/PublicController.java
@@ -5,15 +5,15 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-@ResponseBody
 @Controller
 public class PublicController {
-	@GetMapping("/GWsystem")//系统主页
-	public String mainPage(Model model) {
-		return "admin/index";
-	}
 	
-	@GetMapping("/switchLog")//登录页面
+	@GetMapping("/GWsystem")
+	public String mainPage(Model model) {
+		return "admin/index";//系统主页
+	}
+	//登录页面
+	@GetMapping("/switchLog")
 	public String log(Model model) {
 		return "admin/SwitchLogin";
 	}
