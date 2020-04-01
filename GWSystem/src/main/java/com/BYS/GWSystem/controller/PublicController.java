3d16de1e6d75f302ba0b25fc71c30c7aa9e50154
NorDey <yaozhenbang@icloud.com>
2020-04-01 21:24:58 +0800

    注释

--- GWSystem/src/main/java/com/BYS/GWSystem/controller/PublicController.java
+++ GWSystem/src/main/java/com/BYS/GWSystem/controller/PublicController.java
@@ -16,6 +16,7 @@ public class PublicController {
 	//登录页面
 	@GetMapping("/switchLog")
 	public String log(Model model) {
+		//跳转到公司登录界面的OBJ
 		model.addAttribute("enterprise", new Enterprise());
 		return "Public/SwitchLogin";
 	}
