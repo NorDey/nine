c85a7c3d907b604e1b8983a6ff444c6a14e719f8
leo-yuxiangchao <15171122729@163.com>
2020-04-02 20:41:14 +0800

    完善上传图片功能，添加代码注释

--- GWSystem/src/main/java/com/BYS/GWSystem/controller/GraduateController.java
+++ GWSystem/src/main/java/com/BYS/GWSystem/controller/GraduateController.java
@@ -12,6 +12,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -40,56 +41,81 @@ public class GraduateController {
 		return "graduate/UpdateInfo";
 	}
 
-	@PostMapping("/login") // 毕业生登录
-	public String login(@ModelAttribute Graduate graduate,Model model,HttpSession session) {
-		String rest="";
+	// 毕业生登录
+	@PostMapping("/login")
+	public String login(@ModelAttribute Graduate graduate, Model model, HttpSession session) {
+		String rest = "";// 返回界面地址
+		// 根据学号查询学生信息，看是否存在此学生
 		Long studentId = graduate.getStudentId();
 		Graduate graduates = iGraduateService.queryStudentById(studentId);
-		if(graduates!=null) {
-			if(graduates.getPassword().equals(graduate.getPassword())) {
-				if(graduates.getAvatarPath().equals("")) {
+		if (graduates != null) {
+			// 验证登录密码是否正确
+			if (graduates.getPassword().equals(graduate.getPassword())) {
+				// 未上传头像时，给默认头像
+				if (graduates.getAvatarPath() != null) {
+					if (graduates.getAvatarPath().equals("")) {
+						graduates.setAvatarPath("cs.jpg");
+					}
+				} else {
 					graduates.setAvatarPath("cs.jpg");
 				}
-				model.addAttribute("graduate",graduates);
-				session.setAttribute("studentId", studentId);
-				session.setAttribute("password", graduates.getPassword());
-				rest= "graduate/GraduateHome";
-			}else {
+				// 传graduate对象给前端显示数据
+				model.addAttribute("graduate", graduates);
+				rest = "graduate/GraduateHome";
+			} else {
+				// 传enterprise,graduate对象给前端显示数据
 				model.addAttribute("enterprise", new Enterprise());
-				model.addAttribute("graduate",new Graduate());
-				model.addAttribute("errorInfo","密码错误");
-				rest= "Public/SwitchLogin";
+				model.addAttribute("graduate", new Graduate());
+				// 回传登录失败错误信息
+				model.addAttribute("errorInfo", "密码错误");
+				rest = "Public/SwitchLogin";
 			}
-		}else {
+		} else {
+			// 传enterprise,graduate对象给前端显示数据
 			model.addAttribute("enterprise", new Enterprise());
-			model.addAttribute("graduate",new Graduate());
-			model.addAttribute("errorInfo","学号错误");
-			rest= "Public/SwitchLogin";
+			model.addAttribute("graduate", new Graduate());
+			// 回传登录失败错误信息
+			model.addAttribute("errorInfo", "学号错误");
+			rest = "Public/SwitchLogin";
 		}
 		return rest;
 
 	}
 
-	@GetMapping("/register") // 注册
+	// 注册
+	@GetMapping("/register")
 	public String register() {
 		return "graduate/Register";
 	}
 
-	@PostMapping("/uploadPictures") // 上传毕业生头像
+	// 上传毕业生头像
+	@PostMapping("/uploadPictures/{studentId}")
 	public String uploadPictures(@RequestParam(value = "avatar_file") MultipartFile file,
-			@ModelAttribute Graduate graduate, Model model, HttpServletRequest request) {
+			@ModelAttribute Graduate graduate, Model model, @PathVariable String studentId) {
+		// 获取项目中储存头像的文件夹的绝对路径
 		String path = System.getProperty("user.dir") + "\\src\\main\\resources\\static\\headportrait";
-		String name = file.getOriginalFilename(); // 图片名字
+		// 获得上传的图片名字
+		String name = file.getOriginalFilename();
+		//如果未选择文件，则赋值为初始头像
+		if(name.equals("")) {
+			name="cs.jpg";
+		}
+		// 添加新的头像的文件名
+		graduate.setAvatarPath(name);
+		// 根据前台传过来的毕业生学号，修改数据库图片名
 		int rest = iGraduateService.updatePicByid(graduate);
-		System.out.println(rest);
+		// 拼成头像的完整路径
 		path = path + "\\" + name;
 		try {
+			// 上传文件
 			file.transferTo(new File(path));
 		} catch (IllegalStateException e) {
 			e.printStackTrace();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		return "graduate/updateInfo";
+		Graduate graduates = iGraduateService.queryStudentById(graduate.getStudentId());
+		model.addAttribute("graduate", graduates);
+		return "graduate/GraduateHome";
 	}
 }
