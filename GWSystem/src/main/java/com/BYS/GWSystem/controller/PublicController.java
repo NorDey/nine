4a7c6c1f401a4acb2334f78122a2d7796037e0b5
leo-yuxiangchao <15171122729@163.com>
2020-04-02 20:40:41 +0800

    添加公共的注册响应方法

--- GWSystem/src/main/java/com/BYS/GWSystem/controller/PublicController.java
+++ GWSystem/src/main/java/com/BYS/GWSystem/controller/PublicController.java
@@ -10,21 +10,32 @@ import com.BYS.GWSystem.model.Graduate;
 
 @Controller
 public class PublicController {
-	
+
 	@GetMapping("/GWsystem")
 	public String mainPage(Model model) {
-		return "Public/index";//系统主页
+		return "Public/index";// 系统主页
 	}
-	//登录页面
+
+	// 登录页面
 	@GetMapping("/switchLog")
 	public String log(Model model) {
-		//跳转到公司登录页面的OBJ
+		// 跳转到公司登录页面的OBJ
 		model.addAttribute("enterprise", new Enterprise());
-		//跳转到毕业生登录页面的OBJ
+		// 跳转到毕业生登录页面的OBJ
 		model.addAttribute("graduate", new Graduate());
 		return "Public/SwitchLogin";
-		
+
 	}
-	
-	
+
+	// 注册页面
+	@GetMapping("/switchRegister")
+	public String register(Model model) {
+		// 跳转到公司登录页面的OBJ
+		model.addAttribute("enterprise", new Enterprise());
+		// 跳转到毕业生登录页面的OBJ
+		model.addAttribute("graduate", new Graduate());
+		return "Public/SwitchRegister";
+
+	}
+
 }
