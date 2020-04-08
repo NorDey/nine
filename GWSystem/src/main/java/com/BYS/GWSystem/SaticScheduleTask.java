eae5a22643292e04e34ab37ff38413d2cab879b8
Jerry-zjj <1393424504@qq.com>
2020-04-08 16:17:05 +0800

    热门设置,更改定时器时间

--- GWSystem/src/main/java/com/BYS/GWSystem/SaticScheduleTask.java
+++ GWSystem/src/main/java/com/BYS/GWSystem/SaticScheduleTask.java
@@ -18,7 +18,8 @@ public class SaticScheduleTask {
 	
     //3.添加定时任务
     //或直接指定时间间隔，例如：10秒
-    @Scheduled(fixedRate=10000)
+    //@Scheduled(fixedRate=10000)
+    @Scheduled(cron = "0 */1 * * * ?")
     private void configureTasks() {
        // System.err.println("执行静态定时任务时间: " + LocalDateTime.now());
     	
