f78e3caa0627335c630b75ee0358eee73ec008d2
leo-yuxiangchao <15171122729@163.com>
2020-04-09 12:57:02 +0800

    新增投递简历，查询是否投递简历两个接口

--- GWSystem/src/main/java/com/BYS/GWSystem/mapper/GraduateMapper.java
+++ GWSystem/src/main/java/com/BYS/GWSystem/mapper/GraduateMapper.java
@@ -7,43 +7,48 @@ import org.apache.ibatis.annotations.Param;
 
 import com.BYS.GWSystem.dto.GraduateDto;
 import com.BYS.GWSystem.model.Graduate;
+import com.BYS.GWSystem.model.StudentHistory;
 
 @Mapper
 public interface GraduateMapper {
-		
-		//根据学号查询学生信息
-		public Graduate queryStudentById(String id) ;
-		
-		//修改头像根据id
-		public int updatePicByid(Graduate graduate) ;
-	
-		//查询学生
-		public List<Graduate> selectGraduateList();
-		
-		//更改
-		public int updateGraduate(Graduate graduate);
-		
-		//添加
-		public int insertGraduate(Graduate graduate);
-		
-		//删除
-		public int deleteGraduate(Graduate graduate);
 
-		//查询学生数量
-		public int selectGraduateNumber();
+	// 首次投递简历，insert
+	public int sendCV(String studentId, String postId);
 
-		//未编辑简历学生
-		public List<GraduateDto> selectNotFilledStudent();
+	// 查询此简历是否已投递在此
+	public StudentHistory selectCV(String studentId, String postId);
 
-		//学生列表
-		public List<GraduateDto> selectCheckingStudents();
+	// 根据学号查询学生信息
+	public Graduate queryStudentById(String id);
 
-		//最受欢迎学生列表
-		public List<GraduateDto> selectBestResumeStudents();
+	// 修改头像根据id
+	public int updatePicByid(Graduate graduate);
 
-		//模糊条件查询学生列表
-		public List<GraduateDto> selectGraduateListByMore(@Param(value="lookup") String lookup );
-		
-		
+	// 查询学生
+	public List<Graduate> selectGraduateList();
+
+	// 更改
+	public int updateGraduate(Graduate graduate);
+
+	// 添加
+	public int insertGraduate(Graduate graduate);
+
+	// 删除
+	public int deleteGraduate(Graduate graduate);
+
+	// 查询学生数量
+	public int selectGraduateNumber();
+
+	// 未编辑简历学生
+	public List<GraduateDto> selectNotFilledStudent();
+
+	// 学生列表
+	public List<GraduateDto> selectCheckingStudents();
+
+	// 最受欢迎学生列表
+	public List<GraduateDto> selectBestResumeStudents();
+
+	// 模糊条件查询学生列表
+	public List<GraduateDto> selectGraduateListByMore(@Param(value = "lookup") String lookup);
 
 }
